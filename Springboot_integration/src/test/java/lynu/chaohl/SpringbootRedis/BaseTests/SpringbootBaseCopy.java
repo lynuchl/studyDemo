465f8cbf7c27092e53147cab88d0385f86b25fbd
chaohongliang <chaohongliang@wondersgroup.com>
2024-02-20 15:55:00 +0800

    2024-02-20

--- Springboot_integration/src/test/java/lynu/chaohl/SpringbootRedis/BaseTests/SpringbootBaseCopy.java
+++ Springboot_integration/src/test/java/lynu/chaohl/SpringbootRedis/BaseTests/SpringbootBaseCopy.java
@@ -1,21 +1,23 @@
 package lynu.chaohl.SpringbootRedis.BaseTests;
 
 import lombok.extern.slf4j.Slf4j;
+import lynu.chaohl.SpringbootIntegration.ServerBaseDemo.mapper.mybatisTest.CityMapper;
 import lynu.chaohl.SpringbootIntegration.SpringbootIntegrationApplication;
-import lynu.chaohl.SpringbootIntegration.websocket.service.WebSocketInvokeService;
-//import org.junit.jupiter.api.Test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
+//import org.junit.jupiter.api.Test;
+
 @Slf4j
-//以classes为入口做测试启动类，
-@SpringBootTest(classes = SpringbootBase.class)
-public class SpringbootBase {
+@RunWith(SpringRunner.class)
+//启动类做测试入口，也意味着整个项目会启动
+@SpringBootTest(classes = SpringbootIntegrationApplication.class)
+public class SpringbootBaseCopy {
 
         @Value("${auto.field.title}")
         private String title;
@@ -25,6 +27,9 @@ public class SpringbootBase {
 
         @Value("${auto.field.randomValue}")
         private String randomValue;
+
+        @Resource
+        private CityMapper cityMapper;
 
 
 //        @Autowired
@@ -39,6 +44,7 @@ public class SpringbootBase {
     @Test
     public void Test1(){
         System.out.println("hello Test1");
+        log.info(cityMapper.toString());
     }
 
 //    获取yml配置文件值的测试
